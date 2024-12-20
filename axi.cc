c8b43958dd3b966baf95530e4a7de829933ba6ce
David Sheffield <sheffield.david@gmail.com>
2024-12-20 06:13:31 +0000

    still broken but can single step

--- axi.cc
+++ axi.cc
@@ -41,22 +41,15 @@ std::map<std::string, uint32_t> globals::symtab;
 
 static const uint32_t control = 0xA0050000;
 //static const uint32_t control = 0xA0000000;
-static const uint32_t ram = 0x40e00000;
 
+#define PHYS_ADDR 0x60100000
 #define CONTROL_REG 0
 #define STATUS_REG 1
 #define RAM_REG 2
+#define PC_REG 5
 
-struct status_ {
-  uint32_t state : 3;
-  uint32_t hist : 8;
-  uint32_t bresp : 2;
-  uint32_t hash : 15;  
-  uint32_t read_resp_error : 1;
-  uint32_t write_resp_error : 1;
-  uint32_t read_mismatch : 1;
-  uint32_t rnext : 1;
-};
+#define STEP_MASK (1UL<<16)
+#define STEP_ACK (1UL<<31)
 
 struct rvstatus_ {
   uint32_t ready : 1;
@@ -75,27 +68,11 @@ struct rvstatus_ {
   uint32_t l1d_state : 4;
   uint32_t mem_rsp : 1;
   uint32_t l1i_state : 3;
-  uint32_t l2_state : 4;
+  uint32_t l2_state : 2;
+  uint32_t memq_empty : 1;
 };
 
 static_assert(sizeof(rvstatus_) == 4, "rvstatus bad size");
-
-union status {
-  uint32_t u;
-  status_ s;
-  status(uint32_t u) :u(u) {}
-};
-
-std::ostream &operator<<(std::ostream &out, const status &s) {
-  out << "state       : " <<  s.s.state << "\n";
-  out << "hist        : " << std::hex << s.s.hist << std::dec << "\n";
-  out << "bresp       : " << s.s.bresp << "\n";
-  out << "read error  : " << s.s.read_resp_error << "\n";
-  out << "write error : " << s.s.write_resp_error << "\n";
-  
-  return out;
-}
-
 
 union rvstatus {
   uint32_t u;
@@ -125,12 +102,9 @@ std::ostream &operator<<(std::ostream &out, const rvstatus &rs) {
   out << "l1i state       : " << rs.s.l1i_state << "\n";
   out << "l2  state       : " << rs.s.l2_state << "\n";  
   out << "mem_rsp         : " << rs.s.mem_rsp << "\n";
-  
+  out << "memq_empty      : " << rs.s.memq_empty << "\n";
   return out;
 }
-
-
-static_assert(sizeof(status) == 4, "wrong size for status");
 
 static inline uint8_t *mmap4G() {
   void* mempt = mmap(nullptr, 1UL<<32, PROT_READ | PROT_WRITE,
@@ -154,246 +128,173 @@ inline double timestamp() {
 static Driver *d = nullptr;
 static uint64_t zz = 0, mem_ops = 0;
 
-void sigintHandler(int id) {
+static inline int get_axi_state() {
+  uint32_t states = d->read32(0xd);
+  return ((states>>18) & 7);
+}
+
+static inline void report_status() {
   if(d) {
-    printf("bad addr %x\n", d->read32(7));    
     uint64_t txns = read64(d, 0x3c);
-    uint64_t mem_cycles = read64(d, 0x3e);
-    double avg = static_cast<double>(mem_cycles) / txns;
-    std::cout << "avg = " << avg << "\n";
+    uint32_t states = d->read32(0xd);    
+    rvstatus rs(d->read32(0xa));
+    std::cout << "core state = " << (states & 31) << "\n";
+    std::cout << "l2 state = " << ((states>>5) & 31) << "\n";
+    std::cout << "l1i state = " << ((states>>10) & 15) << "\n";
+    std::cout << "l1d state = " << ((states>>14) & 15) << "\n";
+    std::cout << "axi state = " << ((states>>18) & 7) << "\n";
+    std::cout << rs << "\n";
   }
+
+}
+
+void sigintHandler(int id) {
+  report_status();
   std::cout << "mem_ops = " << mem_ops << "\n";
-  std::cout << "zz = " << zz << "\n";  
+  std::cout << "zz = " << zz << "\n";
   exit(-1);
 }
 
 
+
+void read_bbl(const char *fn, char *mem) {
+  int fd, rc;
+  char *buf;
+  struct stat s;
+  fd = open(fn, O_RDONLY);
+  rc = fstat(fd,&s);
+  buf = (char*)mmap(nullptr, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  assert(buf != reinterpret_cast<void*>(-1L));
+
+  for(size_t i = 0; i < s.st_size; i++) {
+    mem[0x10000+i] = buf[i];
+  }
+  munmap(buf, s.st_size);
+  close(fd);
+}
+
+#define WRITE_WORD(EA,WORD) { *reinterpret_cast<uint32_t*>(c_addr + EA) = WORD; }
+
 int main(int argc, char *argv[]) {
+  uintptr_t pgsize = sysconf(_SC_PAGESIZE);
+  size_t memsize = 1*1024*1024;
+  int fd = open("/dev/mem", O_RDWR | O_SYNC);
+  assert(fd != -1);
   
-  d = new Driver(control);
-  d->write32(CONTROL_REG, 0);
-  d->write32(4, 1);
-  d->write32(4,0);
-  status s(0);
+  int prot = PROT_READ | PROT_WRITE;
+  int flags = MAP_SHARED;
+  void* vaddr = mmap(0,
+		     memsize,
+		     prot,
+		     flags,
+		     fd,
+		     PHYS_ADDR);
+  assert(vaddr != MAP_FAILED);
+  char *c_addr = reinterpret_cast<char*>(vaddr);
+  int *fpga_mem = ((int*)vaddr);
+  //for(int i = 0 ; i < (memsize/4); i++) {
+  //fpga_mem[i] = 0x13;
+  //}
 
-  d->write32(6,ram);
-
-  Driver dd(ram);
-
-  uint8_t *mem_ = mmap4G();
-  char *bin_name = nullptr;
-    
-  if(argc == 2) {
-    bin_name = argv[1];
-  }
-  uint64_t pc = 0;
-  bool isElf = true;
-  pc = load_elf(bin_name, mem_);
-  if(pc == (~0U)) {
-    isElf = false;
-    pc = loadState(mem_, bin_name);
-  }
   
-  //make addresses 0 based
-  d->write32(6,0);
-  uint32_t *mem_w32 = reinterpret_cast<uint32_t*>(mem_);
-  
-  rvstatus rs(d->read32(0xa));
+  int i_pc = 0x1000;
+  int64_t fdt_addr = 0x1000 + (8 * 8);
 
+  read_bbl("bbl.bin", (char*)vaddr);
+  WRITE_WORD(0x1000, 0x297 + 0x10000 - 0x1000);
+  WRITE_WORD(0x1004, 0x597); //1
+  WRITE_WORD(0x1008, 0x58593 + ((fdt_addr - 4) << 20)); //2
+  WRITE_WORD(0x100c, 0xf1402573); //3
+  WRITE_WORD(0x1010, 0x00028067); //4 
+				      
+   d = new Driver(control);
+   d->write32(CONTROL_REG, 0);
+   d->write32(4, 1);
+   d->write32(4, 0);
 
-  //resume pc
-  d->write32(5, pc);
-  printf("initial pc = %x\n", d->read32(5));
+   d->write32(6,PHYS_ADDR);
+   d->write32(8, memsize-1);
 
-  //1 -> bit[0]
-  //2 -> bit[1]
-  //4 -> bit[2]
-  //8 -> bit[3]
-  
-  //syscall emulation is 8
-  d->write32(4, 8 | 2);
-  
-  bool run = true;
-  uint64_t  mem_reads = 0, mem_writes = 0;
-  uint64_t silent_wbs = 0;
-  uint64_t z = 0;
-
-  while(true) {
+   d->write32(PC_REG, 0x1000);
+   __builtin___clear_cache((char*)vaddr, ((char*)vaddr) + memsize);
+   
+   rvstatus rs(d->read32(0xa));
+   while(true) {
     rs.u = d->read32(0xa);
-    if(not(rs.s.ready)) {
+    if(rs.s.ready) {
       break;
     }
-    std::cout << rs;
   }
-  
-  //std::cout << std::hex << d.read32(1) << std::dec << "\n";
-  //exit(-1);
-  signal(SIGINT, sigintHandler);
-  volatile uint32_t *rptr = d->get_ptr(32);
-  volatile uint32_t *wptr = d->get_ptr(36);    
+   
+   //#define DO_STEP
+   uint32_t cr = 8 | 2;
+   //cr |= STEP_MASK;
 
-  uint32_t la = ~0;
-  uint32_t las[8];
-  int ia = 0;
-  int op = ~0;
-  uint64_t l = 0;
-  double start_time = timestamp();
-  while(run) {
-    rs.u = d->read32(0xa);
+   /* let the games begin */
+   d->write32(4, cr);
 
-    if((zz & 63) == 0) {
-      while(d->read32(0x3a) == 0) {
-	int c = d->read32(0x3b);
-	printf("%c", c==0 ? '\n' : c);
-	std::fflush(nullptr);
-	d->write32(0x3a, 1);
-	d->write32(0x3a, 0);
-      }
-    }
-
-    //if(l > (1UL<<24)) {
-    //printf("timeout\n");
-    //break;
-    //}
-
-    //if(rs.s.bad_addr) {
-    //printf("boned!\n");
-    //break;
-    //}
-
-    //l++;
-    if(rs.s.mem_req) {
-      //l = 0;
-      mem_ops++;
-      uint32_t addr = d->read32(15);
-      //la = addr;
-      //las[ia & 7] = addr;
-      //ia++;
-      //op = rs.s.mem_req_opcode;
-      //printf("got req for addr %x, type %d\n",
-      //addr, rs.s.mem_req_opcode);
-      
-      if(rs.s.mem_req_opcode == 4) {
-	++mem_reads;
-	volatile uint32_t *dptr = &mem_w32[(addr/4)];
-	int32x4_t t = vld1q_s32((const int32_t*)(dptr));
-	vst1q_s32((int32_t*)(wptr), t);
-      }
-      else {
-	assert((addr >> 12) != 0);
-	++mem_writes;
-	int32x4_t t = vld1q_s32((const int32_t*)(rptr));
-	volatile uint32_t *dptr = &mem_w32[(addr/4)];
-	vst1q_s32((int32_t*)(dptr), t);
-      }
-      d->write32(CONTROL_REG, 1U<<31);
-      //ack
-      //d->write32(CONTROL_REG, 0);
-    }
-
-    zz++;
-  }
-
-  printf("mem_ops = %lu in %g seconds\n", mem_ops, start_time);
-
-  return 0;
-  
-  uint64_t insns = read64(d, 0x28);
-  uint64_t cycle = read64(d, 0x2a);
-  uint64_t l1i_accesses = read64(d, 0x2c);
-  uint64_t l1i_hits = read64(d, 0x2e);
-  uint64_t l1d_accesses = read64(d, 0x30);
-  uint64_t l1d_hits = read64(d, 0x32);
-  uint64_t l2_accesses = read64(d, 0x34);
-  uint64_t l2_hits = read64(d, 0x36);
-  uint64_t mispredicts = read64(d, 0x38);    
-  std::cout << "insns        = " << insns << "\n";
-  std::cout << "cycle        = " << cycle << "\n";
-  std::cout << "l1i accesses = " << l1i_accesses << "\n";
-  std::cout << "l1i hits     = " << l1i_hits << "\n";
-  std::cout << "l1d accesses = " << l1d_accesses << "\n";
-  std::cout << "l1d hits     = " << l1d_hits << "\n";
-  std::cout << "l2 accesses  = " << l2_accesses << "\n";
-  std::cout << "l2 hits      = " << l2_hits << "\n";    
-  std::cout << "mispredicts  = " << mispredicts << "\n";
-  
-  double ipc = static_cast<double>(insns) / cycle;
-  double mpki = 1000.0 * (static_cast<double>(mispredicts) / insns);
-  std::cout << "ipc   = " << ipc << "\n";
-  std::cout << "mpki  = " << mpki << "\n";
-  std::cout << "mem_writes = " << mem_writes << "\n";
-  std::cout << "mem_reads  = " << mem_reads << "\n";  
-  //std::cout << "silent_wbs = " << silent_wbs << "\n";  
-  munmap(mem_, 1UL<<32);
-  delete d;
-  return 0;
-  
-  while(true) {
-    sleep(1);
-    s.u = d->read32(STATUS_REG);
-    std::cout << "state : " << s.s.state << "\n";
-    std::cout << "hist : " << std::hex << s.s.hist << std::dec << "\n";
-    std::cout << "bresp : " << s.s.bresp << "\n";
-    std::cout << "read_resp_error : " << s.s.read_resp_error << "\n";
-    std::cout << "write_resp_error : " << s.s.write_resp_error << "\n";
-    std::cout << "read_mismatch : " << s.s.read_mismatch << "\n";
-    std::cout << "rnext         : " << s.s.rnext << "\n";
-    std::cout << "zero        : " << s.s.hash << "\n";
-    
-    rs.u = d->read32(0xa);
-    std::cout << rs << "\n";
-    
-    std::cout << std::hex << "memory txns : " << d->read32(14) << std::dec << "\n";
-    std::cout << std::hex << "last addr : " << d->read32(15) << std::dec << "\n";
-
-    if(rs.s.mem_req) {
-      std::cout << "see mem req, force rsp_valid\n";
-      d->write32(CONTROL_REG, 1U<<31);
-      d->write32(CONTROL_REG, 0);
-    }
-    
-    for(int i = 0; i < 4; i++) {
-      std::cout << std::hex << d->read32(16+i) << std::dec << "\n";
-    }
-    
-    if(cpu_stopped(rs)) {
-      std::cout << "EPC : " << std::hex << d->read32(7) << std::dec << "\n";
-      std::cout << "insn retired " << d->read32(0) << "\n";
-      std::cout << std::hex << "memory txns : " << d->read32(14) << std::dec << "\n";
-      std::cout << std::hex << "last addr : " << d->read32(15) << std::dec << "\n";
+  int steps = 0;
+  uint64_t ss = 0;
+  while(1) {
+    if(ss > (1UL<<28)) {
       break;
     }
-    sleep(1);
-  }
-  
-  for(int i = 0; i < 4; i++) {
-    std::cout << std::hex << d->read32(16+i) << std::dec << "\n";
-  }
-  
+    ss++;
+    int v = d->read32(0x3a) & 255;
+    int wptr =v&0xf, rptr = (v>>4)&0xf;
+    if(wptr != rptr) {
+      int c = d->read32(0x3b);	
+      printf("%c", c==0 ? '\n' : c);
+      //printf("%c, w %d, r %d\n", c, wptr, rptr);
+      std::fflush(nullptr);
+      d->write32(0x3a, 1);
+      d->write32(0x3a, 0);
+      ss = 0;
+    }    
 
-
-  
+    if(cr & STEP_MASK) {
+      int state = get_axi_state();
+      if(not(state == 5 || state == 6)) {
+	ss++;
+	continue;
+      }
+      ss = 0;
+      uint32_t cr_ = cr | STEP_ACK;
+      d->write32(4, cr_);
 #if 0
-  Driver dd(ram);
-  std::cout << "dram values interface:\n";  
-  for(int i = 0; i < 1024; i++) {
-    std::cout << dd->read32(i) << "\n";
-  }
+      std::cout
+	<< "state " << state
+	<< " addr " << std::hex << (d->read32(0x8) & (memsize-1))
+	<< " data " << d->read32(0x9)
+	<< std::dec 
+	<< " txns " << d->read32(1)
+	<< "\n";
 #endif
-
-  //std::cout << "last read interface:\n";
-  //d->write32(CONTROL_REG, 1);
-  //d->write32(CONTROL_REG, 0);
+      //report_status();
+      //cr &= (~STEP_MASK);
+      d->write32(4, cr);
+      
+      steps++;
+    }
+    
+    //printf("steps = %d\n", steps);
+  }
   
-  //for(int i = 0;i < 4; i++) {
-  //std::cout << std::hex << d->read32(4+i) << std::dec << "\n";
-  //}
-  
-  //for(int i = 0; i < 4; i++) {
-  //std::cout << std::hex << dd->read32(i) << std::dec << "\n";
-  //}
+
+  printf("last pc %x\n", d->read32(7));    
+
+  report_status();
+
+  std::cout << "axi txns " << d->read32(0x1) << "\n";
+  std::cout << std::hex << "epc : "
+	    << d->read32(0xb) << std::dec << "\n";
 
 
-  
+  std::cout << "last addr " << std::hex << d->read32(0x8) << std::dec << "\n";
+  std::cout << "last data " << std::hex << d->read32(0x9) << std::dec << "\n";  
+  //exit(-1);
+  //signal(SIGINT, sigintHandler);
+
+  munmap(fpga_mem, memsize);
   return 0;
 }
